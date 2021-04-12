d574788eeca6fb0e2dfe2b376a3d69dd128f7903
Oeman1230 <moskalev.iura@mail.ru>
2021-04-12 12:45:57 +0300

    made list with topics and create methods of adding in list

--- src/lession_pack/books/abstClassBook.java
+++ src/lession_pack/books/abstClassBook.java
@@ -6,8 +6,54 @@ public abstract class abstClassBook {
     protected String bookName;
     protected Integer bookYear;
     protected String bookPublisher;
+    public void setBookCode (String code)
+    {
+        bookCode = code;
+    }
+    public void setBookAuthor(String author)
+    {
+        bookAuthor = author;
+    }
+    public void setBookName(String name)
+    {
+        bookName = name;
+    }
+    public void setBookYear(Integer year)
+    {
+        bookYear = year;
+    }
+    public void setBookPublisher(String Publisher) {
+        bookPublisher = Publisher;
+    }
 
     public String getBookCode() {
         return bookCode;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public Integer getBookYear() {
+        return bookYear;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "bookCode='" + bookCode + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", bookName='" + bookName + '\'' +
+                ", bookYear=" + bookYear +
+                ", bookPublisher='" + bookPublisher + '\'' +
+                '}';
     }
 }
--- src/lession_pack/books/bookGuide.java
+++ src/lession_pack/books/bookGuide.java
@@ -2,12 +2,67 @@ package lession_pack.books;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.WeakHashMap;
 
 public class bookGuide extends abstClassBook {
-    protected String guideMainTopic;
-    protected Integer guideTopicsCount;
-    protected List<guideScheme>guideSchemes = new ArrayList<>();
+    bookGuide()
+    {
+        TopicsCount = 0;
+        ShchemesCount = 0;
+    }
+    private String guideMainTopic;
+    protected Integer TopicsCount;
+    protected Integer ShchemesCount;
+    protected List<guideScheme>Schemes = new ArrayList<>();
+    protected List<guideTopic>topics = new ArrayList<>();
 
+    public void addScheme(String schName, Integer schPage, String abstImage, String description)
+    {
+        guideScheme tempScheme = new guideScheme();
+        tempScheme.setSchemeName(schName);
+        tempScheme.setSchemePage(schPage);
+        tempScheme.setSchemeImage(abstImage);
+        tempScheme.setSchemeDescription(description);
+        Schemes.add(tempScheme);
+        ShchemesCount++;
+    }
+    public String getSchemeByPage(Integer page)
+    {
+        Integer i = 0;
+        while (Schemes.iterator().hasNext())
+        {
+
+            if(Schemes.iterator().next().getSchemePage().equals(page))
+            {
+                return Schemes.get(i).getScheme();
+            }
+            i++;
+
+        }
+        return "no such scheme on this page";
+    }
+    public void addTopic(String topName, String topText, Integer topPage)
+    {
+        guideTopic TempTopic = new guideTopic();
+        TempTopic.setTopicName(topName);
+        TempTopic.setTopicText(topText);
+        TempTopic.setTopicStartPage(topPage);
+        topics.add(TempTopic);
+        TopicsCount++;
+    }
+    public String getTopic(Integer page)
+    {
+        Integer i=0;
+        while (topics.iterator().hasNext())
+        {
+            if(topics.iterator().next().getTopicStartPage().equals(page))
+            {
+                return topics.get(i).getTopic();
+            }
+            i++;
+        }
+        return "there is no such topic on this page";
+    }
     //////////////////////////////////////////////////////////////////////////////////////////
     private class guideScheme
     {
@@ -46,6 +101,50 @@ public class bookGuide extends abstClassBook {
         public Integer getSchemePage(){
          return schemePage;
         }
+        public String getScheme() {
+            return "schemeName='" + schemeName + '\'' +
+                    ", schemeImage='" + schemeImage + '\'' +
+                    ", schemeDescription='" + schemeDescription + '\'' +
+                    ", schemePage=" + schemePage +
+                    '}';
+        }
     }
     //////////////////////////////////////////////////////////////////////////////////////////
+    private class guideTopic
+    {
+        private String topicName;
+        private Integer topicStartPage;
+        private String topicText;
+        public void setTopicName(String name)
+        {
+            topicName = name;
+        }
+        public void setTopicStartPage(Integer StartPage)
+        {
+            topicStartPage = StartPage;
+        }
+        public void setTopicText(String text)
+        {
+            topicText = text;
+        }
+        public String getTopicName()
+        {
+            return topicName;
+        }
+        public Integer getTopicStartPage()
+        {
+            return topicStartPage;
+        }
+        public String getTopicText()
+        {
+            return topicText;
+        }
+        public String getTopic() {
+            return
+                    "topicName='" + topicName + '\'' +
+                    ", topicStartPage=" + topicStartPage +
+                    ", topicText='" + topicText + '\'' +
+                    '}';
+        }
+    }
 }
